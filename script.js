ab391ef454fca5cec132e339f78b096cd5aa306d
William Pohlman <william.a.pohlman@gmail.com>
2020-07-25 14:19:22 -0500

    Completed requirements

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ window.addEventListener("load", function(){
    let faultyItems = document.getElementById("faultyItems");
    let fuelStatus = document.getElementById("fuelStatus");
    let cargoStatus = document.getElementById("cargoStatus");
+   let launchStatus = document.getElementById("launchStatus");
+
    form.addEventListener("submit", function(event){
+      
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
@@ -24,16 +27,49 @@ window.addEventListener("load", function(){
          alert("Cargo Mass must be a number");
          event.preventDefault();
       }else{
-         pilotStatus.innerHTML = `${pilotName.value}`;
-         copilotStatus.innerHTML = `${copilotName.value}`;
+         pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch`;
+         copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready for launch`;
          if(Number(fuelLevel.value) < 10000){
             faultyItems.style.visibility = "visible";
             fuelStatus.innerHTML = "Fuel level too low for launch";
-            console.log("Testing");
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = "red";
          }
+         if(Number(cargoMass.value) > 10000){
+            faultyItems.style.visibility = "visible";
+            cargoStatus.innerHTML = "Cargo mass too high for launch";
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = "red";
+         }
+         if(!(launchStatus.style.color === "red")){
+            launchStatus.innerHTML = "Shuttle is ready for launch";
+            launchStatus.style.color = "green";
+         }
+         event.preventDefault();
          
       }
+
+
    });
+
+   let json = [];
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json){
+         let missionTarget = document.getElementById("missionTarget");
+
+         missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[0].name}</li>
+            <li>Diameter: ${json[0].diameter}</li>
+            <li>Star: ${json[0].star}</li>
+            <li>Distance from Earth: ${json[0].distance}</li>
+            <li>Number of Moons: ${json[0].moons}</li>
+         </ol>
+         <img src="${json[0].image}">`;
+      });
+   });
+
 
 });
 
